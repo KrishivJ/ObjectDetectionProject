5d2628098799bfd30a184b98a9b1582969a321f3
KrishivJ <75926502+KrishivJ@users.noreply.github.com>
2021-07-19 12:26:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function sportscar() {
+    window.location = "sportscar.html";
+}
+function street() {
+    window.location = "street.html";
+}
+function electronics_store() {
+    window.location = "electronics_store.html";
+}
+function beach() {
+    window.location = "beach.html";
+}
+function football_field() {
+    window.location = "football_field.html";
+}
